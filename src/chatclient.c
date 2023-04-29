6a28f3035228bba9d706e27bd1e865de9956ac84
katiewimmer <krwim78@gmail.com>
2023-04-29 11:47:26 -0400

    part 4 almost done

--- src/chatclient.c
+++ src/chatclient.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <signal.h>
 #include "util.h"
 //AP server userID: Emmanuel: 1120 Katie: 1362
 //userID Equation: (1000 * x) + (120 OR 362)
@@ -16,15 +17,94 @@ char username[MAX_NAME_LEN + 1];
 char inbuf[BUFLEN + 1];
 char outbuf[MAX_MSG_LEN + 1];
 
+
+/*
+volatile sig_atomic_t running = true;
+
+
+void catch_signal(int sig) {
+    running = false;
+    printf("/n");
+}
+*/
+
 /*Part 4.1*/
 int handle_stdin() {
-    /* TODO * */
-	return 1;
+    
+    char message[MAX_MSG_LEN];
+    memset(message, 0, MAX_MSG_LEN);
+
+    // read input from the user
+    if (fgets(message, MAX_MSG_LEN, stdin) == NULL) {
+        fprintf(stderr, "Error: Read interrupted. %s. \n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Should we be removing the newline?
+    
+    int len = strlen(message);
+    /*
+    if (message[len - 1] == '\n') {
+        message[len - 1] = '\0';
+    }
+    **/
+    // check if message is too long
+    if (len >= MAX_MSG_LEN) {
+        fprintf(stderr, "Sorry, limit your message to 1 line of at most %d characters.\n", MAX_MSG_LEN);
+        // discard the rest of the characters
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF) {}
+    } else {
+        // send the message to the server
+        int ret = send(client_socket, message, strlen(message), 0);
+        if (ret == -1) {
+            fprintf(stderr, "Error: Message failed to send. %s.\n", strerror(errno));
+        }
+
+        // check if the user wants to quit
+        if (strcmp(message, "bye") == 0) {
+            printf("Goodbye.\n");
+            close(client_socket);
+            exit(EXIT_SUCCESS);
+        }
+    }
+    return retval;
 }
 /*Part 4.2*/
 int handle_client_socket() {
-    /* TODO */
-	return 1;
+   	
+    printf("Inside socket connections\n");
+
+    // accept the incoming connection
+    int acc = accept(client_socket, NULL, NULL);
+    if (acc < 0) {
+    	fprintf(stderr, "Error: Unable to accept the incoming connection. %s.\n", strerror(errno));
+    	exit(EXIT_FAILURE);
+    }
+
+    //read the data into inbuf
+    ssize_t num_bytes = read(acc, inbuf, sizeof(inbuf));
+    if (num_bytes < 0 && errno != EINTR) {
+        fprintf(stderr, "Warning: Failed to receive incoming message.\n");
+    } else if (num_bytes == 0) {
+        fprintf(stderr, "\nConnection to server has been lost.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        // print the message from the server
+        inbuf[num_bytes] = '\0';
+        printf("Received message: %s\n", inbuf);
+
+        // check for the "bye" message
+        if (strcmp(inbuf, "bye\n") == 0) {
+            printf("\nServer initiated shutdown.\n");
+            close(client_socket);
+            exit(EXIT_SUCCESS);
+        }
+    }
+    // close the connection
+    close(acc);
+    
+    return retval;
 }
 
 int main(int argc, char **argv) {
@@ -34,6 +114,18 @@ int main(int argc, char **argv) {
         retval = EXIT_FAILURE;
         goto END;
     }
+    
+    /**
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = catch_signal;
+    if (sigaction(SIGINT, &action, NULL) == -1) {
+        fprintf(stderr, "Error: Failed to register signal handler. %s.\n",
+                strerror(errno));
+        return EXIT_FAILURE;
+    }
+    */
+    
     struct sockaddr_in server_addr;
     socklen_t addrlen = sizeof(struct sockaddr_in); //New type
     char *addr_str = argv[1];
@@ -99,8 +191,36 @@ int main(int argc, char **argv) {
         retval = EXIT_FAILURE;
         goto END;
     }
-
-END:
-    return retval;
     
+    //setting up the fd set
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(client_socket, &readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+    
+    while (1){
+    	
+    	//check for initial errors
+    	if (select(FD_SETSIZE, &readfds, NULL, NULL, NULL) < 0) {
+            fprintf(stderr,"Error: Unable to wait for activity. %s.\n", strerror(errno));
+            retval = EXIT_FAILURE;
+            goto END;
+        }
+        
+        //activity on STDIN_FILENO
+        if (FD_ISSET(STDIN_FILENO, &readfds)) {
+        	handle_stdin();
+        }
+        
+        //activity on the socket
+        if (FD_ISSET(client_socket, &readfds)) {
+        	handle_client_socket();
+        }
+    
+    }
+    
+    END:
+    	return retval;   
 }
+
+
